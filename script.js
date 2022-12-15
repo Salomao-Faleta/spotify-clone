e3a86c86bd7cdd64c21e5d6df7077fa9538cfce2
Salomão Faleta <faletas41@gmail.com>
2022-12-14 23:39:48 -0300

    A atualização 0.1
    mudei cores, formatei o menu (Desktop e mobile)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function menuOpener(){
-    let menuArea = document.querySelector('#menuArea');
+    let menuAreaMobile = document.querySelector('#menuAreaMobile');
 
-    if(menuArea.style.width == '150px'){
-        menuArea.style.width = '0px';
+    if(menuAreaMobile.style.width == '150px'){
+        menuAreaMobile.style.width = '0px';
     } else{
-        menuArea.style.width = '150px';
+        menuAreaMobile.style.width = '150px';
     }
 }
 
